9ad2cbf8ec8f6ec1b34e927cdff065ce3051780e
Tranquility <46429872+TranquilityTech@users.noreply.github.com>
2020-10-22 21:14:29 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.json({
+    res.status(200).json({
         res: 200
     });
 });
 
 router.post('/', async (req, res) => {
-    res.json(req.body);
+    res.status(200).json(req.body);
+});
+
+router.post('/login', (req, res, next) => {
+    const token = jwt.sign({
+        name: "test"
+    }, "jwtTokenSecret", {
+        expiresIn: 10
+    });
+    const refreshToken = jwt.sign({
+        name: "test"
+    }, "refreshTokenSecret", {
+        expiresIn: 10
+    })
+    const response = {
+        token,
+        refreshToken
+    }
+
+    tokenList[refreshToken] = response;
+
+    res.status(200).json(response);
 });
 
 

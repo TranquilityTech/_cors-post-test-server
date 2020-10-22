ded4563d8d3120e446ab0edf3bcd84df5514d7a7
Tranquility <46429872+TranquilityTech@users.noreply.github.com>
2020-10-22 19:37:08 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var cors = require("cors")
+
+var app = express();
+
+// enable cors for everyone
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    res.json({
+        res: 200
+    });
+});
+
+app.post('/', async (req, res) => {
+    res.json(req.body);
+});
